98de1aa5993344df6874ffef9824e5374ea72f4d
whitevast <vashal@mail.ru>
2022-01-08 11:21:05 +0800

    - Изменен алгоритм отправки координат в модуль GPS-трекер; - Оптимизирован цикл.

--- modules/pandora/pandora.class.php
+++ modules/pandora/pandora.class.php
@@ -409,27 +409,29 @@ function usual(&$out) {
   if(!isset($data['stats'])) return;
   foreach($devices as $device){
 	  $info = SQLSelect("SELECT * FROM pandora_info WHERE DEVICE_ID='".$device['ID']."'");
+	  $deviceinfo = array_merge($data['stats'][$device['DEV_ID']], $this->parsebit($data['stats'][$device['DEV_ID']]['bit_state_1']));
+	  if($deviceinfo['gps']){
+	    global ${'latitude'.$device['DEV_ID']};
+	    global ${'longitude'.$device['DEV_ID']};
+	    if(${'latitude'.$device['DEV_ID']} != $deviceinfo['x'] or ${'longitude'.$device['DEV_ID']} != $deviceinfo['y']){
+			if($deviceinfo['move']){
+			  ${'latitude'.$device['DEV_ID']} = $deviceinfo['x'];
+			  ${'longitude'.$device['DEV_ID']} = $deviceinfo['y'];
+			  $url = BASE_URL . '/gps.php?latitude=' . $deviceinfo['x']
+			  . '&longitude=' .$deviceinfo['y']
+			  . '&altitude=0'
+			  . '&accuracy=0'
+			  . '&provider=0'
+			  . '&speed='     .$deviceinfo['speed'] 
+			  . '&battlevel=0'
+			  . '&charging=0'
+			  . '&deviceid='  .$device['TITLE']
+			  . '&op=';
+			  getURL($url, 0);
+		   }
+		}
+	  }
 	  foreach($info as $inf){
-		  $deviceinfo = array_merge($data['stats'][$device['DEV_ID']], $this->parsebit($data['stats'][$device['DEV_ID']]['bit_state_1']));
-		  if($deviceinfo['gps']){
-			  global ${'latitude'.$device['DEV_ID']};
-			  global ${'longitude'.$device['DEV_ID']};
-			  if(${'latitude'.$device['DEV_ID']} != $deviceinfo['x'] or ${'longitude'.$device['DEV_ID']} != $deviceinfo['y']){
-				  ${'latitude'.$device['DEV_ID']} = $deviceinfo['x'];
-			      ${'longitude'.$device['DEV_ID']} = $deviceinfo['y'];
-				  $url = BASE_URL . '/gps.php?latitude=' . $deviceinfo['x']
-				  . '&longitude=' .$deviceinfo['y']
-				  . '&altitude=0'
-				  . '&accuracy=0'
-				  . '&provider=0'
-				  . '&speed='     .$deviceinfo['speed'] 
-				  . '&battlevel=0'
-				  . '&charging=0'
-				  . '&deviceid='  .$device['TITLE']
-				  . '&op=';
-				  getURL($url, 0);
-			 }
-		  }
 		  if($inf['TITLE'] == 'balance'){
 			 if($inf['VALUE'] != $deviceinfo['balance']['value']){
 				$params['OLD_VALUE'] = $inf['VALUE'];
